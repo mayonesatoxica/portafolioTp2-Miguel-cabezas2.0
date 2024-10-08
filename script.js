fca49bb16689e31af6aac2eaf6d3cca7faddcba5
miguel <164025473+mayonesatoxica@users.noreply.github.com>
2024-10-08 08:51:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let menuVisible = false;
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("html");
+        habilidades[2].classList.add("css");;
+    }
+}
+window.onscroll = function(){
+    efectoHabilidades();
+} 
